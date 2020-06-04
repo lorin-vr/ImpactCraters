4a202aecfa2b001fcae23b33685979f8bd27a3c0
Lorin van Riel <lorinvanriel@gmail.com>
2020-06-04 14:25:38 +1000

    Initial Commit

---
+++ ImpactCraters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImpactCraters
+//
+//  Created by Lorin on 4/6/20.
+//  Copyright © 2020 Lorin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
